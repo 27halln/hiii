aae8f0db8479ee9f39c0d3d0d5a0a9527210efe8
MotorTruck1221 <motortruck1221@protonmail.com>
2024-06-22 03:38:30 -0600

    Format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,30 @@
 import node from '@astrojs/node';
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import playformCompress from '@playform/compress';
 import robotsTxt from 'astro-robots-txt';
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
-import icon from "astro-icon";
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://incog.nebula.christmas',
-  integrations: [tailwind(), robotsTxt(), sitemap(), playformCompress({
-    CSS: false,
-    HTML: true,
-    Image: true,
-    JavaScript: true,
-    SVG: true
-  }), icon()],
-  output: 'server',
-  adapter: node({
-    mode: 'middleware'
-  })
+    site: 'https://incog.nebula.christmas',
+    integrations: [
+        tailwind(),
+        robotsTxt(),
+        sitemap(),
+        playformCompress({
+            CSS: false,
+            HTML: true,
+            Image: true,
+            JavaScript: true,
+            SVG: true
+        }),
+        icon()
+    ],
+    output: 'server',
+    adapter: node({
+        mode: 'middleware'
+    })
 });
