2ed7ced06b165872a32adf8505ef69eaa0730423
MotorTruck1221 <motortruck1221@protonmail.com>
2024-06-26 00:25:39 -0600

    Make the thing feel better and fix up some weird stuff

--- public/register.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.ready.then(async () => {
-        //await registerRemoteListener(sw.active!)
-        console.log('Service Worker Ready');
-        await window.loadProxyScripts();
-        await window.setTransport("epoxy");
-    });
-    navigator.serviceWorker.register('/sw.js', { scope: '/' })
-}
---
+++ src/ts/serviceWorker.ts
@@ -0,0 +1,18 @@
+function initServiceWorker() {
+    return new Promise<void>((resolve) => {
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.ready.then(async () => {
+                //await registerRemoteListener(sw.active!)
+                console.log('Service Worker Ready');
+                //@ts-ignore
+                await window.loadProxyScripts();
+                //@ts-ignore
+                await window.setTransport("epoxy");
+                resolve();
+            });
+            navigator.serviceWorker.register('/sw.js', { scope: '/' });
+        }
+    })
+}
+
+export { initServiceWorker }
