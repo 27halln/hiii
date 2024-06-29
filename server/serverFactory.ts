276a9caaba40e7cf9ca0b793cc63c0e98cd84a1b
MotorTruck1221 <motortruck1221@protonmail.com>
2024-06-29 04:57:15 -0600

    Add the ability to use rammerhead

--- server/serverFactory.ts
+++ server/serverFactory.ts
@@ -1,44 +1,41 @@
 import { createServer } from 'node:http';
 import { createBareServer } from '@tomphttp/bare-server-node';
 import { routeRhRequest, routeRhUpgrade, shouldRouteRh } from './rammerhead';
-const bare = createBareServer('/bare/');
 import wisp from 'wisp-server-node';
+
+function initBareServer() {
+    if (process.env.BARE_SERVER_ENABLED === 'true') {
+        const bare = createBareServer('/bare/');
+        return bare;
+    }
+}
+
+const bare = initBareServer();
+
 const serverFactory = (handler: any) => {
     return createServer()
         .on('request', (req: any, res: any) => {
             if (process.env.BARE_SERVER_ENABLED === 'true') {
-                if (bare.shouldRoute(req)) {
-                    bare.routeRequest(req, res);
-                } else if (shouldRouteRh(req)) {
-                    routeRhRequest(req, res);
-                } else {
-                    handler(req, res);
-                }
-            } else {
-                if (shouldRouteRh(req)) {
-                    routeRhRequest(req, res);
-                } else {
-                    handler(req, res);
-                }
+                if (bare?.shouldRoute(req)) { bare?.routeRequest(req, res) }
+            }
+            if (shouldRouteRh(req)) {
+                routeRhRequest(req, res);
+            }
+            else {
+                handler(req, res)
             }
         })
         .on('upgrade', (req: any, socket: any, head: any) => {
             if (process.env.BARE_SERVER_ENABLED === 'true') {
-                if (bare.shouldRoute(req)) {
-                    bare.routeUpgrade(req, socket, head);
-                } else if (shouldRouteRh(req)) {
-                    routeRhUpgrade(req, socket, head);
-                } else if (req.url.endsWith('/wisp/')) {
-                    //@ts-ignore
-                    wisp.routeRequest(req, socket, head);
+                if (bare?.shouldRoute(req)) {
+                    bare?.routeUpgrade(req, socket, head)
                 }
-            } else {
-                if (shouldRouteRh(req)) {
-                    routeRhUpgrade(req, socket, head);
-                } else {
-                    //@ts-ignore
-                    wisp.routeRequest(req, socket, head);
-                }
+            }
+            if (shouldRouteRh(req)) {
+                routeRhUpgrade(req, socket, head)
+            }
+            if (req.url?.endsWith('/wisp/')) {
+                wisp.routeRequest(req, socket, head);
             }
         });
 };
