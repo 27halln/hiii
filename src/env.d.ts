9166f15142e16d6c5ac51e2ba935d109188e94d3
MotorTruck1221 <motortruck1221@protonmail.com>
2024-07-02 04:12:11 -0600

    Other silly types

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,29 +1,9 @@
 /// <reference path="../.astro/env.d.ts" />
 /// <reference types="astro/client" />
 /// <reference types="@titaniumnetwork-dev/ultraviolet/client" />
-declare global {
-    interface Window {
-        openApp(url: string): void;
-        searchApps(val: string): void;
-        setTheme(val: string): void;
-        setProxy(val: string): void;
-        transport(val: string): void;
-        changeTitle(val: string): void;
-        changeFavicon(val: string): void;
-        searchGames(val: string): void;
-        fullScreenGame(val: string): void;
-        loadProxyScripts(): Promise;
-        setTransport(transport: string): Promise;
-        loadMobileNav(): void;
-        closeMobileNav(): void;
-        createLink(): void;
-        exitIframe(): void;
-        refreshIframe(): void;
-        setTitle(): void;
-        __uv: any;
-        __get$ProxyUrl: any;
-    }
-}
-
-//Force TS to recognize this as a module
-export {}
+declare var search: (input: string, template: string) => any;
+type Suggestion = {
+    phrase: string;
+};    
+declare var BareMux: any;
+declare var EpxMod: any;
---
+++ src/global.d.ts
@@ -0,0 +1,25 @@
+declare global {
+    interface Window {
+        openApp(url: string): void;
+        searchApps(val: string): void;
+        setTheme(val: string): void;
+        setProxy(val: string): void;
+        transport(val: string): void;
+        changeTitle(val: string): void;
+        changeFavicon(val: string): void;
+        searchGames(val: string): void;
+        fullScreenGame(val: string): void;
+        loadProxyScripts(): Promise;
+        setTransport(transport: string): Promise;
+        loadMobileNav(): void;
+        closeMobileNav(): void;
+        createLink(): void;
+        exitIframe(): void;
+        refreshIframe(): void;
+        setTitle(): void;
+        __uv: any;
+        __get$ProxyUrl: any;
+    }
+}
+
+export {};
